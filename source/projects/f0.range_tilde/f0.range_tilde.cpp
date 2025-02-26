9f41ec83bd7a149a7d1e894796601be83b514e85
redFrik <f@fredrikolofsson.com>
2025-02-26 18:25:23 +0100

    f0.range fixes

--- source/projects/f0.range_tilde/f0.range_tilde.cpp
+++ source/projects/f0.range_tilde/f0.range_tilde.cpp
@@ -46,11 +46,11 @@ public:
             cout << "TODO check if these are set: " << max_arg <<endl;
             cout << "TODO check flags: " << m_flags << endl;
             if (m_flags == 0) {
-                m_min = <double>(INFINITY);
-                m_max = <double>(-INFINITY);
+                m_min = C74_INT64_MAX;
+                m_max = C74_INT64_MIN;
             } else if (m_flags == 1) {
                 m_min = m_setMin;
-                m_max = <double>(-INFINITY);
+                m_max = C74_INT64_MIN;
             } else {
                 m_min = m_setMin;
                 m_max = m_setMax;
@@ -100,10 +100,10 @@ public:
 
 private:
     short m_flags { 0 };
-    double m_max { <double>(-INFINITY) };
-    double m_min { <double>(INFINITY) };
-    double m_setMax { <double>(-INFINITY) };
-    double m_setMin { <double>(INFINITY) };
+    double m_max { C74_INT64_MIN };
+    double m_min { C74_INT64_MAX };
+    double m_setMax { C74_INT64_MIN };
+    double m_setMin { C74_INT64_MAX };
     
 };
 
