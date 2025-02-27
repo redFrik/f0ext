df125677a021e1c693fe3e3f3127f4634d8924c7
redFrik <f@fredrikolofsson.com>
2025-02-27 18:16:17 +0100

    Update f0.wrap.cpp

--- source/projects/f0.wrap/f0.wrap.cpp
+++ source/projects/f0.wrap/f0.wrap.cpp
@@ -29,13 +29,13 @@ public:
     f0_wrap(const atoms& args = {}) {
         if (args.size() == 1) {
             max = args[0];
-            if (args[0].type() == c74::min::int_argument) {
+            if (args[0].type() == int_argument) {
                 m_intswitch = false;
             }
         } else if (args.size() == 2) {
             min = args[0];
             max = args[1];
-            if ((args[0].type() == c74::min::int_argument) || (args[1].type() == c74::min::int_argument)) {
+            if ((args[0].type() == int_argument) || (args[1].type() == int_argument)) {
                 m_intswitch = false;
             }
         }
